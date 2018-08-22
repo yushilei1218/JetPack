0e40c31e44eddfd74d9a733c8d39400c002ef953
shilei.yu <shilei.yu@zhaopin.com.cn>
2018-08-22 10:17:55 +0800

    新增ignore

---
+++ app/src/main/java/com/test/shileiyu/jetpack/common/widget/PagerIndicator.java
@@ -0,0 +1,23 @@
+package com.test.shileiyu.jetpack.common.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * @author shilei.yu
+ * @date 2018/8/20
+ */
+
+public class PagerIndicator extends LinearLayout {
+
+    public PagerIndicator(Context context) {
+        super(context);
+    }
+
+    public PagerIndicator(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
