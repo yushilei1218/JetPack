bcc6fed114c6edae8132b8571cfe7da0c0ab703f
shilei.yu <shilei.yu@zhaopin.com.cn>
2018-09-12 15:40:48 +0800

    test SlidePaneLayout

--- app/src/main/java/com/test/shileiyu/jetpack/ui/BottomSheetBehaviorActivity.java
+++ app/src/main/java/com/test/shileiyu/jetpack/ui/BottomSheetBehaviorActivity.java
@@ -85,8 +85,8 @@ public class BottomSheetBehaviorActivity extends BaseActivity {
         slideLayout.setPanelSlideListener(new SlidingPaneLayout.PanelSlideListener() {
             @Override
             public void onPanelSlide(@NonNull View panel, float slideOffset) {
-                int width = slideLv.getWidth();
-                slideLv.setTranslationX(slideOffset * width - width);
+//                int width = slideLv.getWidth();
+//                slideLv.setTranslationX(slideOffset * width - width);
             }
 
             @Override
