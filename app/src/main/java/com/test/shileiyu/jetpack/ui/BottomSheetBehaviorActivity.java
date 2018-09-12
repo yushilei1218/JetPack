1efb06051962b64ad2c947a4878195bb91704f36
shilei.yu <shilei.yu@zhaopin.com.cn>
2018-09-12 15:31:16 +0800

    test SlidePaneLayout

--- app/src/main/java/com/test/shileiyu/jetpack/ui/BottomSheetBehaviorActivity.java
+++ app/src/main/java/com/test/shileiyu/jetpack/ui/BottomSheetBehaviorActivity.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.SwipeDismissBehavior;
+import android.support.v4.widget.SlidingPaneLayout;
 import android.view.View;
+import android.widget.ListView;
 
 import com.test.shileiyu.jetpack.R;
+import com.test.shileiyu.jetpack.common.adapter.BeanAdapter;
 import com.test.shileiyu.jetpack.common.base.BaseActivity;
+import com.test.shileiyu.jetpack.common.bean.Bean;
 import com.test.shileiyu.jetpack.common.util.Util;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -16,10 +23,20 @@ import butterknife.OnClick;
 import static android.support.design.widget.BottomSheetBehavior.STATE_COLLAPSED;
 import static android.support.design.widget.BottomSheetBehavior.STATE_EXPANDED;
 import static android.support.design.widget.BottomSheetBehavior.STATE_HIDDEN;
+import static android.support.design.widget.SwipeDismissBehavior.SWIPE_DIRECTION_START_TO_END;
 
+/**
+ * @author shilei.yu
+ */
 public class BottomSheetBehaviorActivity extends BaseActivity {
     @BindView(R.id.bottom_sheet_layout)
     View bottom;
+    @BindView(R.id.swipe_dismiss)
+    View swipe;
+    @BindView(R.id.lv_slide)
+    ListView slideLv;
+    @BindView(R.id.slide_pane)
+    SlidingPaneLayout slideLayout;
 
     private int[] stateArr = new int[]{
             STATE_COLLAPSED,
@@ -31,6 +48,21 @@ public class BottomSheetBehaviorActivity extends BaseActivity {
 
     @Override
     protected void initView(Bundle savedInstanceState) {
+        CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) swipe.getLayoutParams();
+        SwipeDismissBehavior swipeDismissBehavior = new SwipeDismissBehavior();
+        swipeDismissBehavior.setSwipeDirection(SWIPE_DIRECTION_START_TO_END);
+        swipeDismissBehavior.setListener(new SwipeDismissBehavior.OnDismissListener() {
+            @Override
+            public void onDismiss(View view) {
+                Util.showToast("onDismiss");
+            }
+
+            @Override
+            public void onDragStateChanged(int state) {
+
+            }
+        });
+        lp.setBehavior(swipeDismissBehavior);
         mBehavior = BottomSheetBehavior.from(bottom);
         mBehavior.setHideable(true);
         mBehavior.setPeekHeight(Util.dp2px(this, 50));
@@ -43,6 +75,28 @@ public class BottomSheetBehaviorActivity extends BaseActivity {
             @Override
             public void onSlide(@NonNull View bottomSheet, float slideOffset) {
 
+            }
+        });
+
+        BeanAdapter adapter = new BeanAdapter();
+        adapter.data = Bean.getList();
+        slideLv.setAdapter(adapter);
+
+        slideLayout.setPanelSlideListener(new SlidingPaneLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(@NonNull View panel, float slideOffset) {
+                int width = slideLv.getWidth();
+                slideLv.setTranslationX(slideOffset * width - width);
+            }
+
+            @Override
+            public void onPanelOpened(@NonNull View panel) {
+                Util.showToast("onPanelOpened");
+            }
+
+            @Override
+            public void onPanelClosed(@NonNull View panel) {
+                Util.showToast("onPanelClosed");
             }
         });
     }
